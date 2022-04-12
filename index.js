9dcd1118deaacab849c7d23603949c5aaafe4c89
Wahyu Budi Saputra <wsaputra@binaracademy.org>
2022-04-13 00:18:10 +0800

    Implementasi register dan tampilkan json

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Anggap saja sebuah database
+const users = [];
+
 app.set("view engine", "ejs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -19,6 +25,28 @@ app.get("/salam", (req, res) => {
 
 app.get("/register", (req, res) => {
   res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  users.push({
+    email: email,
+    password: password,
+  });
+
+  console.log(users);
+
+  res.redirect("/register");
+});
+
+app.get("/jumlah-user", (req, res) => {
+  res.send(`Jumlah user ${users.length}`);
+});
+
+app.get("/tampilkan-user-json", (req, res) => {
+  res.json(users);
 });
 
 app.listen(port, () => {
